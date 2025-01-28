5e7f7cfb2f40394a55916c4d098a0f700ac4cb7b
Divine Aghulor <divineaghulor04@gmail.com>
2025-01-28 18:40:14 +0100

    commit 1.1 updated port to use env variable so it works with render

--- index.js
+++ index.js
@@ -48,9 +48,11 @@ app.get('/data', async (req, res) => {
   });
 
 
-  app.listen(3000, ()=>{
-    console.log("Listening on port 3000");
-    
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+
   })
 
 
